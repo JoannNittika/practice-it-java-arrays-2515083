e97bbfff4a2278c30163053f2d432b4c1484e97b
JoannNittika <54028931+JoannNittika@users.noreply.github.com>
2024-01-31 09:11:50 +0000

    working with arrays chapter 1.1

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 public class App {
   
   public static void main(String[] args) {
-    
+    double[] lotteryNums = {45,70,89,69,32};//new double[5];
+    System.out.println(lotteryNums[2]);
+    lotteryNums[2] = 55;
+    System.out.println(lotteryNums[2]);
   }
 }
